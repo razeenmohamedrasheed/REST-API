054a25a3b9a0fc38d5fc7bb8afba750daaa7a3ec
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2025-03-23 21:57:25 +0530

    Task API created

--- model/users.py
+++ model/users.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
 
 class User(Base):
@@ -12,3 +13,16 @@ class User(Base):
     password = Column(String(255), nullable=False)         # Use String for hashed passwords
     created_at = Column(DateTime, default=func.now())      # Added creation timestamp
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())  # Auto update timestamp
+    
+    tasks = relationship("Tasks", back_populates="user", cascade="all, delete-orphan")
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+    task_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    username = Column(String(50), nullable=False)
+    taskname = Column(String(500), nullable=False)
+    created_at = Column(DateTime, default=func.now())      # Added creation timestamp
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())  # Auto update timestamp
+
+    user = relationship("User", back_populates="tasks")
