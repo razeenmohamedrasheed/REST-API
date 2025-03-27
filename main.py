ec1714861926ef534e7300a3ac15e1a7bb9c295a
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2025-03-27 19:50:40 +0530

    cors added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api.endpoints import registration, tasklist, file_data
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 
@@ -9,6 +10,13 @@ app.include_router(registration.router)
 app.include_router(tasklist.router)
 app.include_router(file_data.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
